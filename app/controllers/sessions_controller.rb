49cc862dd602c504da172a57072a85ef0b520a0e
Inframatic <isteffy@hotmail.com>
2013-07-29 16:57:18 -0400

    Correcting paths for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
-  def new
+  def new	
+  	@user = User.new
   end
 
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to products_url, :notice => "Logged in!"
+      redirect_to users_url, :notice => "Logged in!"
     else
       flash.now[:alert] = "Invalid email or password"
       render "new"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Entreprenologyproject::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
 resources :users
-
+resources :sessions, :only => [:new, :create, :destroy]
 end
