54832014ee5bf599698951842226b3d09c9e7ce0
Inframatic <isteffy@hotmail.com>
2013-07-29 14:30:57 -0400

    Refactored the users controller to respond to JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users}
+    end
   end
 
   def show
     @user = User.find(params[:id])
+
+      respond_to do |format|
+      format.html
+      format.json { render json: @users}
+    end
   end
 
   def edit
@@ -13,30 +23,47 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+
+      respond_to do |format|
+      format.html
+      format.json { render json: @users}
+    end
   end
 
   def create
     @user = User.new(params[:user])
 
-    if @user.save
-      redirect_to users_url
-    else
-      render :new
-    end
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
   end
 
   def update
     @user = User.find(params[:id])
 
-    if @user.update_attributes(params[:user])
-      redirect_to product_path(@user)
-    else
-      render :edit
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+
+      respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
   end
 end
