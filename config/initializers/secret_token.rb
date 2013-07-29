04c65abf8e63856ee43b0b4e1d140599df82fb04
Inframatic <isteffy@hotmail.com>
2013-07-29 13:00:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Entreprenologyproject::Application.config.secret_key_base = 'b03088c30037b4a52fad942c52e83807afeac27f8ef41e4e674a0dcdc17975e4730b62e9fb7f984958a1b21472cce3afa85df782963d778fe5c965e112f14ea3'
