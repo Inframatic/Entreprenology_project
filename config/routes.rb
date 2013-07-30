d56737ef550e4c02450c907016ee4f367314caa9
Brad Wilcox <brad_wilcox@me.com>
2013-07-30 15:17:44 -0400

    Refactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Entreprenologyproject::Application.routes.draw do
 resources :users
+resources :users
 resources :sessions, :only => [:new, :create, :destroy]
 
-post 'startups' => "startups#create"
-get 'startups' => 'startups#index'
-get 'startups/new' => 'startups#new'
-get 'startups/:id' => 'startups#show', as: "startup"
-
-get 'startups/:id/edit' => "startups#edit", as: "edit_startup"
-put 'startups/:id' => "startups#update", as: "update_startup"
+root :to => "startups#index"
 
 end
