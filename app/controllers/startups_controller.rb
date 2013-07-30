845282c236cb3d61da9be90e0ccf6e1ca1b212d2
Brad Wilcox <brad_wilcox@me.com>
2013-07-30 13:16:11 -0400

    matching the HTTP Requests with actions in the StartupsController

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,24 +1,49 @@
 class StartupsController < ApplicationController
 
   def index
+    @startup = Startup.all
   end
 
   def show
+    @startup = Startup.find(params[:id])
   end
+
 
   def edit
+    @startup = Startup.find(params[:id])
   end
+
 
   def new
+    @startup = Startup.new
   end
+
 
   def create
+    @startup = Startup.new(params[:startup])
+    if 
+      @startup.save
+      redirect_to startup_url
+    else
+      render :new
+    end
   end
+
 
   def update
+    @startup = Startup.find(params[:id])
+      if 
+        @startup.update_attributes(params[:startup])
+        redirect_to startup_path(@startup)
+      else
+        render :edit
+      end
   end
 
+
   def destroy
+    @startup = Startup.find(params[:id])
+    @startup.destroy
   end
 
 end 
