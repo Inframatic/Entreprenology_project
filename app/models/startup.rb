483242d58147409468772108f315f5e41db5a294
Brad Wilcox <brad_wilcox@me.com>
2013-07-30 15:48:40 -0400

    adding validation in startup.rb

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,2 +1,4 @@
 class Startup < ActiveRecord::Base
+  validates :company_name, :contact_person, :contact_person_email, :industry, :city, :country, :presence => true
+  validates :number_of_employees, :numericality => {:only_integer => true}
 end
