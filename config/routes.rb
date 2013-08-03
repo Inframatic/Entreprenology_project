dd729f04297b255107fd900690224ff26559d08a
Brad Wilcox <brad_wilcox@me.com>
2013-08-03 08:56:14 -0400

    refactoring layout

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Entreprenologyproject::Application.routes.draw do
 
 end
 
-
-
   # get "log_in" => "sessions#new", :as => "log_in"
   # get "log_out" => "sessions#destroy", :as => "log_out"
   # get "sign_up" => "users#new", :as => "sign_up"
