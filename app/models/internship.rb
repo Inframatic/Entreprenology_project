71bd50b6d449f9cbb73ace6e4915013165d97b55
Brad Wilcox <brad_wilcox@me.com>
2013-08-07 21:50:08 -0400

    Fixing a bug(conflict) since merge with making_rails_api, in internship model file.

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,10 +1,6 @@
 class Internship < ActiveRecord::Base
 
-<<<<<<< HEAD
-  belongs_to :startup
-=======
   belongs_to :startup, :dependent => :destroy
->>>>>>> making_rails_api
   has_one :internship_contact
 
   validates :start_date, :end_date, :position_description, :presence => true
